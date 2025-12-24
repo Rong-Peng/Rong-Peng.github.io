60b31b12a2d91bf74b6ecc616498082c7393ef39
Tina <tina@TinadeMacBook-Pro.local>
2025-12-24 18:06:04 +0800

    更新图片和index.html

--- images.js
+++ images.js
@@ -1,28 +1,78 @@
 // 图片数据配置
 const mockImages = [
-  {
+    {
     id: 1,
-    url: 'https://cdn.jsdelivr.net/gh/Rong-Peng/Rong-Peng.github.io@main/ass/pho/bk1.png',
-    title: 'AIGC 作品 1',
-    description: '尺寸：2048x2048'
+    url: '/ass/pho/bj7.jpg',
+    title: '商品图 1',
+    description: '尺寸： 1280x1280'
   },
-  {
+    {
     id: 2,
-    url: 'https://cdn.jsdelivr.net/gh/Rong-Peng/Rong-Peng.github.io@main/ass/pho/bg2.png',
-    title: 'AIGC 作品 2',
-    description: '尺寸：2048x2048'
+    url: '/ass/pho/bj8.png',
+    title: '商品图 2',
+    description: '尺寸： 2048x2048'
   },
+
   {
     id: 3,
-    url: 'https://cdn.jsdelivr.net/gh/Rong-Peng/Rong-Peng.github.io@main/ass/pho/bg3.png',
-    title: 'AIGC 作品 3',
-    description: '尺寸：2752x1536'
+    url: 'https://cdn.jsdelivr.net/gh/Rong-Peng/Rong-Peng.github.io@main/ass/pho/bg2.png',
+    title: '商品图 3',
+    description: '尺寸：2048x2048'
   },
   {
     id: 4,
     url: 'https://cdn.jsdelivr.net/gh/Rong-Peng/Rong-Peng.github.io@main/ass/pho/bk1.png',
-    title: 'AIGC 作品 4',
+    title: '商品图 4',
+    description: '尺寸：2048x2048'
+  }, 
+  {
+    id: 5,
+    url: '/ass/pho/bj4.jpg',
+    title: '商品图 5',
+    description: '尺寸： 2048x2048'
+  },
+
+  {
+    id: 6,
+    url: 'https://cdn.jsdelivr.net/gh/Rong-Peng/Rong-Peng.github.io@main/ass/pho/bg3.png',
+    title: '商品图 6',
     description: '尺寸：2752x1536'
+  },
+  {
+    id: 7,
+    url: 'https://cdn.jsdelivr.net/gh/Rong-Peng/Rong-Peng.github.io@main/ass/pho/bk1.png',
+    title: '商品图 7',
+    description: '尺寸：2752x1536'
+  },
+   {
+    id: 8,
+    url:'/ass/pho/bg5.jpg',
+    title: '商品图 8',
+    description: '尺寸： 1344x768'
+  },
+  {
+    id: 9,
+    url:'/ass/pho/bg9.png',
+    title: '商品图 9',
+    description: '尺寸： 1344x768'
+  },
+  {
+    id: 10,
+    url:'/ass/pho/bj12.png',
+    title: '商品图 10',
+    description: '尺寸： 1344x768'
+  },
+  {
+    id: 11,
+    url:'/ass/pho/bj11.jpeg',
+    title: '商品图 11',
+    description: '尺寸： 900X900'
+  },
+    {
+    id: 12,
+    url:'/ass/pho/bj12.png',
+    title: '商品图 12',
+    description: '尺寸： 2752x1536'
   },
   // 在下面继续添加更多图片
   // {
