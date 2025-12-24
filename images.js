40754e36b5f08a014c4bd309f11f6a7cdf9253a2
Tina <tina@TinadeMacBook-Pro.local>
2025-12-24 14:18:15 +0800

    Add waterfall modal, download and external images.js

---
+++ images.js
@@ -0,0 +1,34 @@
+// 图片数据配置
+const mockImages = [
+  {
+    id: 1,
+    url: 'ass/pho/bg1.png',
+    title: 'AIGC 作品 1',
+    description: '尺寸：2048x2048'
+  },
+  {
+    id: 2,
+    url: 'ass/pho/bg2.png',
+    title: 'AIGC 作品 2',
+    description: '尺寸：2048x2048'
+  },
+  {
+    id: 3,
+    url: 'ass/pho/bg3.png',
+    title: 'AIGC 作品 3',
+    description: '尺寸：2752x1536'
+  },
+  {
+    id: 4,
+    url: 'ass/pho/bk1.png',
+    title: 'AIGC 作品 4',
+    description: '尺寸：2752x1536'
+  },
+  // 在下面继续添加更多图片
+  // {
+  //   id: 3,
+  //   url: 'image-url.png',
+  //   title: '图片标题',
+  //   description: '图片描述'
+  // },
+];
